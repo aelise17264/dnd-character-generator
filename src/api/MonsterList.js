a0530ede29c5857a75b3da51b8044bac40ebe35e
Anna <aelise17264@gmail.com>
2023-09-05 15:37:30 -0600

    progress on displaying monsters, refresh needs fix

--- src/api/MonsterList.js
+++ src/api/MonsterList.js
@@ -1,43 +1,166 @@
 export const monsterList = [
-  "goblin",
-  "giant-rat",
-  "dust-mephit",
-  "ice-mephit",
-  "magma-mephit",
-  "steam-mephit",
-  "bandit",
-  "gray-ooze",
-  "spy",
-  "flying-snake",
-  "mimic",
-  "animated-armor",
-  "harpy",
-  "grimlock",
-  "zombie",
-  "skeleton",
-  "dire-wolf",
-  "kobold",
-  "shambling-mound",
-  "sahuagin",
-  "ghoul",
-  "gnoll",
-  "orc",
-  "lizardfolk",
-  "ankheg",
-  "cult-fanatic",
-  "gibbering-mouther",
-  "black-dragon-wyrmling",
-  "blue-dragon-wyrmling",
-  "brass-dragon-wyrmling",
-  "bronze-dragon-wyrmling",
-  "copper-dragon-wyrmling",
-  "gold-dragon-wyrmling",
-  "green-dragon-wyrmling",
-  "red-dragon-wyrmling",
-  "silver-dragon-wyrmling",
-  "white-dragon-wyrmling",
-  "awakened-shrub",
-  "green-hag",
-  "imp",
-  "giant-centipede",
+  {
+    index: "goblin",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/30783/955/1000/1000/638062024584880857.png",
+  },
+  {
+    index: "giant-rat",
+    img: "https://www.dndbeyond.com/avatars/70/273/636463305245423768.png",
+  },
+  {
+    index: "dust-mephit",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/34175/931/1000/1000/638190241938100148.png",
+  },
+  {
+    index: "ice-mephit",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/34175/909/1000/1000/638190241405142934.png",
+  },
+  {
+    index: "magma-mephit",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/31079/74/1000/1000/638074037477949301.png",
+  },
+  {
+    index: "steam-mephit",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/34175/946/1000/1000/638190242330361682.png",
+  },
+  {
+    index: "bandit",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/0/181/1000/1000/636252761965117015.jpeg",
+  },
+  {
+    index: "gray-ooze",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/30834/153/1000/1000/638063882595415196.png",
+  },
+  {
+    index: "spy",
+    img: "https://www.dndbeyond.com/attachments/2/656/humanoid.jpg",
+  },
+  {
+    index: "flying-snake",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/16/517/1000/1000/636376319640572721.jpeg",
+  },
+  {
+    index: "mimic",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/30833/408/1000/1000/638063863232165584.png",
+  },
+  {
+    index: "animated-armor",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/31312/506/1000/1000/638084408333616236.png",
+  },
+  {
+    index: "harpy",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/30788/677/1000/1000/638062178962566030.png",
+  },
+  {
+    index: "grimlock",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/30784/270/1000/1000/638062035227436078.png",
+  },
+  {
+    index: "zombie",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/30836/823/1000/1000/638063945376881949.png",
+  },
+  {
+    index: "skeleton",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/30835/849/1000/1000/638063922565505819.png",
+  },
+  {
+    index: "dire-wolf",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/16/484/1000/1000/636376300478361995.jpeg",
+  },
+  {
+    index: "kobold",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/30832/207/1000/1000/638063832924455756.png",
+  },
+  {
+    index: "shambling-mound",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/30835/836/1000/1000/638063922372165925.png",
+  },
+  {
+    index: "sahuagin",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/30835/789/1000/1000/638063921569706098.png",
+  },
+  {
+    index: "ghoul",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/14/483/1000/1000/636364323937041514.png",
+  },
+  {
+    index: "gnoll",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/30784/505/1000/1000/638062044385041691.png",
+  },
+  {
+    index: "orc",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/30834/160/1000/1000/638063882785865067.png",
+  },
+  {
+    index: "lizardfolk",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/30832/479/1000/1000/638063839674591734.png",
+  },
+  {
+    index: "ankheg",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/30761/865/1000/1000/638061096692582271.png",
+  },
+  {
+    index: "cult-fanatic",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/0/259/1000/1000/636252768307230924.jpeg",
+  },
+  {
+    index: "gibbering-mouther",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/30784/554/1000/1000/638062046140139699.png",
+  },
+  {
+    index: "black-dragon-wyrmling",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/16/496/1000/1000/636376308217017511.jpeg",
+  },
+  {
+    index: "blue-dragon-wyrmling",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/21222/242/1000/1000/637708181281501343.jpeg",
+  },
+  {
+    index: "brass-dragon-wyrmling",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/30782/475/1000/1000/638061963543984454.png",
+  },
+  {
+    index: "bronze-dragon-wyrmling",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/16/492/1000/1000/636376306909593829.jpeg",
+  },
+  {
+    index: "copper-dragon-wyrmling",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/30782/536/1000/1000/638061965633505640.png",
+  },
+  {
+    index: "gold-dragon-wyrmling",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/30782/562/1000/1000/638061966416208867.png",
+  },
+  {
+    index: "green-dragon-wyrmling",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/30782/393/1000/1000/638061960809474996.png",
+  },
+  {
+    index: "red-dragon-wyrmling",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/30782/412/1000/1000/638061961411525220.png",
+  },
+  {
+    index: "silver-dragon-wyrmling",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/30782/730/1000/1000/638061973263583027.png",
+  },
+  {
+    index: "white-dragon-wyrmling",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/30782/441/1000/1000/638061962420535703.png",
+  },
+  {
+    index: "awakened-shrub",
+    img: "https://i.imgur.com/abNTNG1.png",
+  },
+  {
+    index: "green-hag",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/30788/618/1000/1000/638062177566175594.png",
+  },
+  {
+    index: "imp",
+    img: "https://www.dndbeyond.com/avatars/thumbnails/30782/21/1000/1000/638061948417487055.png",
+  },
+  {
+    index: "giant-centipede",
+    img: "https://www.vhv.rs/dpng/d/441-4413114_dungeons-and-dragons-giant-centipede-hd-png-download.png",
+  },
 ];
--- src/components/Monsters.js
+++ src/components/Monsters.js
@@ -6,34 +6,47 @@ import { faSkull } from "@fortawesome/free-solid-svg-icons";
 import { monsterList } from "../api/MonsterList";
 
 import "./Components.css";
+import { act } from "@testing-library/react";
 
 function Monsters() {
-  const [monsterData, setMonsterData] = useState([]);
+  let [monsterData, setMonsterData] = useState([]);
+  let [monsterImage, setMonsterImage] = useState("");
   // let armorType;
 
   const baseURL = "https://www.dnd5eapi.co/api/monsters/";
   function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
   }
+
+  const initialState = {
+    
+  }
+
   const getMonster = () => {
-    const monsterIndex = randomNumber(1, 40);
-    const ourMonster = monsterList[monsterIndex];
+    generateMonster();
+    // if(monsterData.length > 0){
+    //   monsterData = ""
+    // }
+    const monsterIndex = randomNumber(0, 40);
+    const ourMonster = monsterList[monsterIndex].index;
+    setMonsterImage(monsterList[monsterIndex].img);
     axios
       .get(baseURL + ourMonster)
       .then((res) => {
         let monsterDetails = res.data;
         // console.log(monsterDetails)
         setMonsterData(monsterDetails);
+        console.log("description", monsterDetails.desc)
 
         // console.log(action.name)
-       
-        let armorClass = monsterDetails.armor_class
-        getArmor(armorClass)
+
+        let armorClass = monsterDetails.armor_class;
+        getArmor(armorClass);
 
         let actions = monsterDetails.actions;
         getActions(actions);
-        let speed = monsterDetails.speed
-        getSpeed(speed)
+        let speed = monsterDetails.speed;
+        getSpeed(speed);
       })
       .catch((err) => {
         console.log("something went wrong", err);
@@ -41,10 +54,14 @@ function Monsters() {
   };
 
   const getActions = (actions) => {
+    console.log(actions)
+    if(actions == undefined){
+      return ""
+    }
     actions.forEach((action) => {
       let li = document.createElement("li");
       console.log(action);
-      let text = document.createTextNode(action.name + ": " + action.desc)
+      let text = document.createTextNode(action.name + ": " + action.desc);
       li.appendChild(text);
       document.getElementById("monsterActions").appendChild(li);
     });
@@ -52,38 +69,67 @@ function Monsters() {
 
   const getArmor = (armorClass) => {
     // let para = document.createElement('p')
-    let text = document.createElement("p")
-    if(armorClass[0].armor !== undefined){
-      console.log('inside if', armorClass[0].armor[0].name)
-      let armorType = armorClass[0].armor[0].name
+    console.log(armorClass)
+    if(armorClass == undefined){
+      return ""
+    }
+    let text = document.createElement("p");
+    if (armorClass[0].armor !== undefined) {
+      console.log("inside if", armorClass[0].armor[0].name);
+      let armorType = armorClass[0].armor[0].name;
+      let armorValue = armorClass[0].value;
 
-      text = document.createTextNode("Armor Type: " + armorType)
-
-    }else{
-      console.log('inside else')
-      console.log(armorClass[0].type)
-     let naturalArmor = armorClass[0].type
-     text = document.createTextNode("Armor Type: " + naturalArmor)
+      text = document.createTextNode("Armor Type: " + armorType + " " + armorValue);
+    } else {
+      console.log("inside else");
+      console.log(armorClass[0].type);
+      let naturalArmor = armorClass[0].type;
+      let naturalValue = armorClass[0].value
+      text = document.createTextNode("Armor Type: " + naturalArmor + " " + naturalValue);
     }
     // console.log('final type', armorType)
     // return armorType
     // innerHTML("Armor:" + text)
     document.getElementById("armorType").appendChild(text);
+  };
 
+  const getSpeed = (speedy) => {
+    if(speedy == undefined){
+      return ""
+    }
+    console.log("speed", speedy);
+    let speedList = document.createElement("p");
+    for (var key in speedy) {
+      console.log(key, speedy[key]);
+      let speed = document.createTextNode(key + ": " + speedy[key] + " | ");
+      speedList.appendChild(speed);
+      document.getElementById("speedList").appendChild(speedList);
+    }
+  };
+  function generateMonster() {
+    // let newCharName
+    document.getElementById("monsterDetails").style.display = "flex";
+    // console.log(newCharList[newCharIndex])
+    // return newCharName
   }
 
-const getSpeed = (speedy) => {
-  console.log('speed', speedy)
-  let speedList = document.createElement("p")
-  for(var key in speedy){
-    console.log(key, speedy[key])
-    let speed = document.createTextNode(key + ": " + speedy[key] + ", ")
-    speedList.appendChild(speed)
-    document.getElementById("speedList").appendChild(speedList)
+  const resetMonster = () => {
+    monsterData = useState(initialState)
+    getActions(monsterData.actions)
+    getArmor(monsterData.armor_class)
+    getSpeed(monsterData.speed)
+  }
+
+  const refreshPage = () => {
+
+    window.location.reload(false)
+    // document.getElementById("monsterDetails").style.display = "none";
 
   }
-}
-
+  const callRefresh = () => {
+    resetMonster()
+    getMonster()
+  }
 
   return (
     <div className="monsterPage">
@@ -108,35 +154,71 @@ const getSpeed = (speedy) => {
             />
           </a>
         </button>
-      </div>
-      <div className="monsterDetails">
-        <h2>{monsterData.name}</h2>
-        <div id="monsterSpeed">
-          {/* <p>{monsterData.speed.toString()}</p> */}
-        </div>
-        <p>{monsterData.desc}</p>
-        <p>Type: {monsterData.type}</p>
-        <p>Language: {monsterData.languages}</p>
-        <p>Alignment: {monsterData.alignment}</p>
-        <p>Hit Points: {monsterData.hit_points}</p>
-        <h3>Speed</h3>
-        <div id="speedList"></div>
-        <div id="armorType"></div>
-        <img src={monsterData.image}></img>
-        <h3>Actions</h3>
-        <div id="monsterActions"></div>
-        <div className="monsterStats">
-          <h3>Stats</h3>
-          <ul>
-            <li>Str: {monsterData.strength}</li>
-            <li>Dex: {monsterData.dexterity}</li>
-            <li>Con: {monsterData.constitution}</li>
-            <li>Int: {monsterData.intelligence}</li>
-            <li>Wis: {monsterData.wisdom}</li>
-            <li>Char: {monsterData.charisma}</li>
-          </ul>
-        </div>
        
+      </div>
+      <div
+        className="monsterDetails"
+        id="monsterDetails"
+        style={{ display: "none" }}
+      >
+            <h1>{monsterData.name}</h1>
+
+       
+        <div className="monsterGroups">
+          <div className="monsterGroup1">
+          <div className="monsterImage">
+          <img src={monsterImage}></img>
+        </div>
+        <div className="monsterStats">
+            <div id="monsterSpeed">
+              {/* <p>{monsterData.speed.toString()}</p> */}
+            </div>
+            <p>{monsterData.desc}</p>
+            <p>Type: {monsterData.type}</p>
+            <p>Language: {monsterData.languages}</p>
+            <p>Alignment: {monsterData.alignment}</p>
+            <p>Hit Points: {monsterData.hit_points}</p>
+            <h3>Speed</h3>
+            <div id="speedList"></div>
+            <div id="armorType"></div>
+            </div>
+          </div>
+
+          <div className="monsterGroup2">
+            <h3>Actions</h3>
+            <div id="monsterActions"></div>
+            <div className="monsterStats">
+              <h3>Stats</h3>
+              <ul>
+                <li>Str: {monsterData.strength}</li>
+                <li>Dex: {monsterData.dexterity}</li>
+                <li>Con: {monsterData.constitution}</li>
+                <li>Int: {monsterData.intelligence}</li>
+                <li>Wis: {monsterData.wisdom}</li>
+                <li>Char: {monsterData.charisma}</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <button
+          type="button"
+          className="btn btn-primary btn-lg"
+          style={{
+            backgroundColor: "#282c34",
+            border: "none",
+            width: "175px",
+            marginBottom: "2%",
+          }}
+          onClick={callRefresh}
+        >
+          Don't Like It
+          <a>
+            <FontAwesomeIcon
+              icon={faSkull}
+              style={{ color: "white", marginLeft: "7px" }}
+            />
+          </a>
+        </button>
       </div>
     </div>
   );
